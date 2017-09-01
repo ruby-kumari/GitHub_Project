78fb6c777600166a51ae305375f65bea5e1fdab5
ruby <ruby@DESKTOP-B06N28O>
2017-08-31 19:43:49 -1200

    First code

---
+++ src/hello/abc.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class abc {
+
+	private int id;
+	private String name;
+	@Override
+	public String toString() {
+		return "abc [id=" + id + ", name=" + name + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
